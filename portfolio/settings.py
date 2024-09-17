d98f11d6633bcd2b7a10c65ec4b001a12dda8f2f
Murat <murat.dublin@gmail.com>
2024-09-17 05:52:06 +0100

    Configured app settings for Azure deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,15 +1,14 @@
 import os
-import django_heroku
-import dj_database_url
 from pathlib import Path
+from azure.storage.blob import BlobServiceClient
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'zr0-y-x6f0e#karwjos7u%)&q+c$la_n)gz)yyh&bdy7h6b34f')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['muratportfolio.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['muratportfolio.azurewebsites.net', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -23,12 +22,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -52,11 +50,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://USER:PASSWORD@localhost:5432/dbname',
-        conn_max_age=600,
-        ssl_require=os.environ.get('DATABASE_URL') is not None
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -75,29 +72,20 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
 
-django_heroku.settings(locals())
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-    },
-}
+AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
